d21d60361369bfe3243556cdfd45a414aba48d3e
dinoxido <130578197+dinoxido@users.noreply.github.com>
2023-09-04 11:48:46 -0300

    payttt

---
+++ introducao-javascript/js/index.js
@@ -0,0 +1,6 @@
+//busca no site pelo armazenamento r dentro da variavel titilo
+let titulo = document.querySelector(".titulo");
+//mostra os devtools de texto da variavel para fins de teste
+console.log(titulo.textContent);
+//muda ostexto da variavel titulo para o texto desejado
+titulo.textContent ="Fica Grande Nutrição"
